aaeee03b0ab575854e80d90c4def25a78614560f
ilham <ilham@smktelkom-mlg.com>
2026-02-06 14:22:40 +0700

    fix bug prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,8 +2,11 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
-    }
+export class PrismaService extends PrismaClient
+  implements OnModuleInit {
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Connected to DB:', process.env.DATABASE_URL);
+  }
 }
